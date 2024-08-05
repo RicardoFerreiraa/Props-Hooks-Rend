3fc655c6c917ab51bb69b84694d316a9f006b531
Ricardo S. Ferreira <ricardo.silva_ferreira@hotmail.com>
2024-08-05 15:06:09 -0300

    Props - hooks - Rend

---
+++ src/components/Contador/Contador.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import './Contador.css'
+
+function Contador() {
+
+  //let valor: number = 0;
+
+  //Variavel de estado
+  const[valor, setValor] = useState(0);
+
+  function somarMaisUm(){
+    setValor(valor +1);
+  }
+
+  console.log(valor);
+  return (
+    <div className="container">
+        <p>O valor é: {valor}</p>
+        <button onClick={somarMaisUm}>Adicionar +1</button>
+    </div>
+  )
+}
+
+export default Contador
